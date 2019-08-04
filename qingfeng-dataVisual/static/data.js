ca2ffec4a9a009fbf6221f08de056f127295ddfe
QingFengKuangBao <jiachao4416@gmail.com>
2019-08-04 11:16:20 +0800

    数据可视化上传

---
+++ qingfeng-dataVisual/static/data.js
@@ -0,0 +1,137 @@
+var all_data = new Map()
+var count=100
+function getDataFile() {
+    var file = document.querySelector('#data_file').files[0]
+    var d_year = []
+    var category = []
+    var data = []
+    var mesg
+    // console.log(file)
+    var reader = new FileReader()
+    reader.readAsText(file, 'utf-8')
+    reader.onload = function (e) {
+        // console.log(this)
+        document.querySelector('.upload').style.display = 'none'
+        lines = this.result.split('\n')
+        // console.log(lines)
+        for (let i in lines) {
+            if (lines[i] == '') {
+                break
+            }
+            if (i == 0) {
+                d_year = lines[i].split(',')
+                mesg=d_year[0]
+                temp_year = []
+                temp_year.push('')
+                for (let yi = 1; yi < d_year.length - 1; yi++) {
+                    geni = 0
+                    start = d_year[yi]
+                    while (true) {
+                        geni++
+                        temp_year.push(start)
+                        if (geni >= count) {
+                            break
+                        }
+                    }
+
+                }
+                // console.log(temp_year)
+
+            } else {
+                l = lines[i].split(',')
+                temp = []
+                temp.push(l[0])
+                for (let li = 1; li < l.length - 1; li++) {
+                    start = l[li]
+                    end = l[li + 1]
+                    geni = 0
+                    let space = Math.floor(eval(end - start) / count)
+                    while (true) {
+                        temp.push(parseInt(start) + space * geni)
+                        geni++
+                        if (geni >= count) {
+                            break
+                        }
+                    }
+                }
+                category.push(l[0])
+                data.push(temp)
+                // console.log(temp)
+
+                // ls_line = lines[i].split(',')
+                // category.push(ls_line[0])
+                // data.push(ls_line)
+            }
+        }
+
+
+        all_data.set('category', category)
+        all_data.set('d_yaer', temp_year)
+        all_data.set('data', data)
+        all_data.set('mesg', mesg)
+        // console.log(all_data)
+        dealData()
+    }
+
+}
+
+function dealData() {
+    var xy_map = new Map()
+    var category = all_data.get('category')
+    var d_yaer = all_data.get('d_yaer')
+    var data = all_data.get('data')
+    // console.log(data)
+    for (let i = 1; i < d_yaer.length; i++) {
+        var y_label = []
+        var x_data = []
+        var origi_data = []
+        var data = all_data.get('data')
+        for (let datai in data) {
+            x_data.push(data[datai][i])
+            origi_data.push(data[datai][i])
+        }
+
+        // console.log(x_data)
+
+
+        x_data.sort(function (a, b) {
+            return a - b
+        })
+        for (let xi in x_data) {
+            y_label.push(category[origi_data.indexOf(x_data[xi])])
+        }
+        // console.log(y_label)
+        if (xy_map.has(d_yaer[i])) {
+            xy_map.get(d_yaer[i]).push({
+                'x_data': x_data,
+                'y_label': y_label
+            })
+        } else {
+            xy_map.set(d_yaer[i], [{
+                'x_data': x_data,
+                'y_label': y_label
+            }])
+        }
+    }
+    // console.log(xy_map)
+
+    // 对数据进行排序
+    var arrayObj = Array.from(xy_map);
+    // console.log(arrayObj)
+    arrayObj.sort(function (a, b) {
+        return a[0].localeCompare(b[0])
+    })
+    // console.log(arrayObj)
+    xy_map.clear()
+    xy_map = new Map(arrayObj.map(i => [i[0], i[1]]))
+
+    // console.log(xy_map)
+    //生成最终的数据
+    final_data = new Map()
+    final_data.set('category', category)
+    final_data.set('data', xy_map)
+    final_data.set('mesg',all_data.get('mesg'))
+
+    // console.log(final_data)
+    draw(final_data)
+}
---
+++ qingfeng-dataVisual/static/main.js
@@ -0,0 +1,232 @@
+// 初始化各个柱状图的颜色
+var colorMap = new Map()
+var timeOut=80
+function initColorMap(category) {
+    var colorSet=new Set()
+    while(true){
+        var c='rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')'
+        // console.log(c)
+        colorSet.add(c)
+        if(colorSet.size>=category.length){
+            break
+        }
+    }
+    var i=0;
+    var values=colorSet.values()
+    while(true){
+        // console.log(c)
+        colorMap.set(category[i],values.next().value)
+        i++
+        if(i>=category.length){
+            break
+        }
+    }
+    console.log(colorMap)
+}
+
+// 基于准备好的dom，初始化echarts实例
+var myChart
+
+var option = {
+    title: {
+        text: '',
+        left: 'center',
+        // top:'middle',
+        textStyle: {
+            color: "black",
+            fontStyle: 'normal',
+            fontWeight: 'bold',
+            fontSize: 30,
+            lineHeight: 40,
+            // width:200,
+            // textAlign:'right',
+            left: 'center',
+        },
+    },
+    tooltip: {
+        show: true,
+        trigger: 'axis',
+        textAlign: 'center',
+    },
+
+    xAxis: {
+        position: 'top',
+        type: 'value',
+        // name: '人口增长率\n       %',
+        // axisLabel: {
+        //     formatter: '{value}'
+        // },
+        axisTick: {
+            show: false,
+        },
+        axisLine: {
+            show: false,
+        },
+        axisLabel: {
+            show: false,
+        },
+        nameTextStyle: {
+            color: "black",
+            fontStyle: 'normal',
+            fontWeight: 'bold',
+            fontSize: 18,
+            // lineHeight: 40,
+            // width:200,
+            // textAlign:'right',
+            // left: 'center',
+        }
+    },
+    yAxis: {
+        type: 'category',
+        axisTick: {
+            show: false,
+        },
+        axisLine: {
+            show: false,
+        },
+
+        data: [],
+        axisLabel: {
+            width: 150,
+            show: true,
+            fontSize: 20,
+            fontWeight: 'bold',
+            rotate: -10,
+
+            color: function (params) {
+                // console.log(params)
+                return colorMap.get(params)
+            },
+            margin: 8,
+            ailgn: 'center',
+            backgroundColor: 'auto',
+            padding: 5,
+
+            // borderColor :'gray',
+            // borderWidth :2,
+            // borderRadius :8,
+
+            textBorderColor: 'black',
+            textBorderWidth: 2,
+            textShadowColor: 'gray',
+            textShadowBlur: 5,
+            textShadowOffsetX: 3,
+            textShadowOffsetY: 5,
+
+        },
+
+        // nameTextStyle: {
+        //     // color: function (params) {
+        //     //     return colorMap.get(params.name)
+        //     // },
+        //     backgroundColor :'red',
+
+        // },
+    },
+    series: [{
+            // barWidth: 50,
+            data: [],
+            type: 'bar',
+            itemStyle: {
+                color: function (params) {
+                    return colorMap.get(params.name)
+                },
+                borderWidth: 2,
+                borderColor: 'black',
+                barBorderRadius: 10,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+                shadowBlur: 10,
+                shadowOffsetX: 7,
+                shadowOffsetY: 7,
+                opacity: 0.8,
+            },
+            emphasis: {
+
+            },
+            label: {
+                // rotate :-10,
+                offset: [10, 0],
+                show: true,
+                position: 'right',
+                fontSize: 18,
+                color: function (params) {
+                    return colorMap.get(params.name)
+                },
+
+                borderColor: 'auto',
+                borderWidth: '2px',
+                padding: 5,
+                borderRadius: 10,
+                shadowColor: 'auto',
+                shadowBlur: 50,
+                // fontWeight:'bold',
+                // formatter: '{c',
+                // lineHeight:25,
+                // backgroundColor:'gray',
+                textBorderColor: 'black',
+                textBorderWidth: 2,
+
+            },
+            barCategoryGap: '50%',
+        },
+
+    ],
+}
+
+
+function initChart(chart_data, title) {
+    option.title.text = title
+    option.yAxis.data = chart_data.get('y_label')
+    // console.log(chart_data['y_label'])
+    // console.log(chart_data['x_data'])
+    option.series[0]['data'] = chart_data.get('x_data')
+    myChart.setOption(option);
+}
+
+
+
+function draw(all_data) {
+    myChart = echarts.init(document.getElementById('mychart'));
+    var mesg=all_data.get('mesg')
+    var i = 0
+    var category = all_data.get('category')
+    var year_data = all_data.get('data')
+    // console.log(year_data)
+    var arr = Array.from(year_data)
+    // console.log(arr)
+    initColorMap(category)
+    var i=0
+    var ei=0
+    // console.log(arr[0][1])
+    data_len=arr[0][1][0]['x_data'].length
+    var c=window.setInterval(function(){
+        chart_data = new Map()
+        data=arr[i]
+        // console.log(data)
+        var title=data[0]+mesg
+        e_data=data[1]
+        if(data_len>10){
+            x_data = []
+            y_label = []
+            for (var datai = data_len - 10; datai < data_len; datai = datai + 1) {
+                x_data.push(e_data[ei]['x_data'][datai])
+                y_label.push(e_data[ei]['y_label'][datai])
+            }
+            chart_data.set('x_data', x_data)
+            chart_data.set('y_label', y_label)
+        }else{
+            chart_data.set('x_data', e_data[ei]['x_data'])
+            chart_data.set('y_label', e_data[ei]['y_label'])
+        }
+        initChart(chart_data,title)
+        ei++
+        if(ei>=data[1].length){
+            ei=0
+            i++
+        }
+        if(ei>=data[1].length&i>=arr.length){
+            window.clearInterval(c)
+        }
+    },timeOut)
+
+}
