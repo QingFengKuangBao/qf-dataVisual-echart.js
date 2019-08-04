7b7367efd0563d4915e4cd58a12ed444e5d51c5c
QingFengKuangBao <jiachao4416@gmail.com>
2019-08-04 22:40:09 +0800

    添加注释

--- qingfeng-dataVisual/static/config.js
+++ qingfeng-dataVisual/static/config.js
@@ -1,3 +1,12 @@
-var genDataCount=20
-var timeOut=120
-var barCount=5
+//两组数据之间 自动生成填充多沙组数据
+var genDataCount=80
+
+//设置绘制间隔 尽量不要修改
+var timeOut=70
+
+//生成多少各柱状  例如：需要排名前10的数据  则设置为10
+var barCount=20
+
+//保留数据的精度，保留小数点后几位数字，
+//根据你的数据设置合适的精度
+var decimal=2
