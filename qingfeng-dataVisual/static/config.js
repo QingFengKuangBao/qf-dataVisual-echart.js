f438097003e66634d00c92f76f9127943ab5b001
jiachao4416@gmail.com <123jiachao>
2019-11-07 23:05:18 +0800

    更改 var-> let

--- qingfeng-dataVisual/static/config.js
+++ qingfeng-dataVisual/static/config.js
@@ -1,18 +1,18 @@
 //两组数据之间 自动生成填充多沙组数据
-var genDataCount=60
+let genDataCount=60
 
 //设置绘制间隔 尽量不要修改
-var timeOut=90
+let timeOut=90
 
 //生成多少各柱状  例如：需要排名前10的数据  则设置为10
-var barCount=20
+let barCount=20
 
 //保留数据的精度，保留小数点后几位数字，
 //根据你的数据设置合适的精度
-var decimal=0
+let decimal=0
 
 //柱状图的颜色映射  colorMap.set('中国'，'color')
-var colorMap = new Map()
-var fontsize=0
+let colorMap = new Map()
+let fontsize=0
 
 
